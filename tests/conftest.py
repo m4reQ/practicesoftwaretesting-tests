6a453fe66845b69ab0824577bdb126818809a12f
m4reQ <m.mizerski2004@gmail.com>
2026-04-19 20:50:16 +0200

    Add shopping cart creation fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import string
 import random
+import requests
 from selenium.webdriver import Firefox
 
 @pytest.fixture(scope='function')
@@ -13,3 +14,10 @@ def driver():
 @pytest.fixture(scope='function')
 def random_email():
     return ''.join(random.choice(string.ascii_letters) for _ in range(8)) + '@gmail.com'
+
+@pytest.fixture(scope='function')
+def shopping_cart():
+    response = requests.post('https://api.practicesoftwaretesting.com/carts')
+    assert response.status_code == 200
+
+    return response.json()['id']
