34dbb95d53e92a51154d72c8f8612758fbcff3da
m4reQ <m.mizerski2004@gmail.com>
2026-05-06 13:50:20 +0200

    Add positive API test case for ST3

--- tests/api/test_api_st3.py
+++ tests/api/test_api_st3.py
@@ -34,7 +34,7 @@ def test_pt3_2():
 
 def test_pt3_3():
     # NOTE API should reject request with incomplete body
-    
+
     send_response = requests.post(
         'https://api.practicesoftwaretesting.com/messages',
         json={
@@ -43,3 +43,50 @@ def test_pt3_3():
             'message': 'Testowa wiadomość zawierająca więcej niż 50 znaków dotycząca wybranego tematu.'})
     assert send_response.status_code == 422
     assert send_response.json().get('email') is not None
+
+def test_pt3_ad1(admin_user: str):
+    # arrange
+    response = requests.post(
+        'https://api.practicesoftwaretesting.com/messages',
+        json={
+            'name': 'Hoo Lee Sheet',
+            'email': 'hoolee.sheet@chingbong.com',
+            'subject': 'payments',
+            'message': 'Czy mogę płacić wientamskimi dongami? Nie mam innej waluty a potrzebuję thor hammer, koniecznie na jutro. Proszę o odpowiedź to dla mnie ważne'})
+    assert response.status_code == 200
+    
+    response = requests.get(
+        'https://api.practicesoftwaretesting.com/messages',
+        headers={
+            'Authorization': f'Bearer {admin_user}',
+        })
+    assert response.status_code == 200
+
+    messages = response.json()['data']
+    assert len(messages) > 0
+
+    message_id = messages[0]['id']
+
+    # act
+    response = requests.put(
+        f'https://api.practicesoftwaretesting.com/messages/{message_id}/status',
+        headers={
+            'Authorization': f'Bearer {admin_user}',
+        },
+        json={
+            'status': 'RESOLVED',
+        })
+    
+    # assert
+    assert response.status_code == 200
+
+    response = requests.get(
+        'https://api.practicesoftwaretesting.com/messages',
+        headers={
+            'Authorization': f'Bearer {admin_user}',
+        })
+    assert response.status_code == 200
+
+    messages = response.json()['data']
+    assert len(messages) > 0
+    assert any(x['id'] == message_id and x['status'] == 'RESOLVED' for x in messages)
