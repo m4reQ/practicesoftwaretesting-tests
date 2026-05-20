2cb699255182e5014a1057497474388a43bb8a4f
Emersonle0 <lukasz.polednik04@gmail.com>
2026-05-20 12:01:15 +0200

    Add behave tests for ST4

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,7 @@
-from selenium.webdriver import Firefox
+from selenium.webdriver import ActionChains, Firefox
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support.expected_conditions import WebElement, visibility_of_element_located
+from selenium.webdriver.support.expected_conditions import WebElement, visibility_of_all_elements_located, visibility_of_element_located
 
 
 class MainPage:
@@ -35,3 +35,59 @@ class MainPage:
     def add_currently_visible_item(self) -> None:
         self.driver.find_element(By.ID, 'btn-add-to-cart').click()
         WebDriverWait(self.driver, 3.0).until(lambda _driver: len(_driver.find_element(By.ID, 'toast-container').find_elements(By.XPATH, './/*')) > 0)
+
+    def input_search_query(self, query_text: str) -> None:
+        search_query_input: WebElement = WebDriverWait(self.driver, 3.0).until(visibility_of_element_located((By.ID, 'search-query')))
+        search_query_input.send_keys(query_text)
+
+    def click_search_button(self) -> None:
+        search_button: WebElement = WebDriverWait(self.driver, 3.0).until(visibility_of_element_located((By.XPATH, '//button[@data-test=\'search-submit\']')))
+        search_button.click()
+    
+    def get_results_search_query(self) -> str:
+        search_term: WebElement = WebDriverWait(self.driver, 3.0).until(visibility_of_element_located((By.XPATH, '//span[@data-test=\'search-term\']')))
+        return search_term.text
+    
+    def all_search_results_contain_search_query(self, query_text: str) -> bool:
+        WebDriverWait(self.driver, 3.0).until(visibility_of_element_located((By.XPATH, '//div[@data-test=\'search_completed\']')))
+    
+        product_titles: list[WebElement] = WebDriverWait(self.driver, 3.0).until(visibility_of_all_elements_located((By.XPATH, '//*[@data-test=\'product-name\']')))
+        return all(query_text.lower() in x.text.lower() for x in product_titles)
+    
+    def has_any_products(self) -> bool:
+        WebDriverWait(self.driver, 3.0).until(visibility_of_element_located((By.XPATH, '//div[@data-test=\'filter_completed\']')))
+
+        product_titles: list[WebElement] = WebDriverWait(self.driver, 3.0).until(visibility_of_all_elements_located((By.XPATH, '//*[@data-test=\'product-name\']')))
+        return len(product_titles) > 0
+    
+    def select_price_range(self) -> None:
+        price_slider: WebElement = WebDriverWait(self.driver, 3.0).until(visibility_of_element_located((By.CLASS_NAME, 'ngx-slider-pointer-max')))
+        price_slider_bar: WebElement = WebDriverWait(self.driver, 3.0).until(visibility_of_element_located((By.XPATH, '//span[contains(@class, \'ngx-slider-full-bar\')]')))
+
+        self.driver.execute_script('arguments[0].scrollIntoView();', price_slider)
+
+        ActionChains(self.driver).drag_and_drop_by_offset(
+            price_slider,
+            -price_slider_bar.rect['width'] / 4,
+            0).perform()
+        
+        self.driver.implicitly_wait(3.0)
+        
+        max_price_label: WebElement = WebDriverWait(self.driver, 3.0).until(visibility_of_element_located((By.XPATH, '//span[contains(@class, \'ngx-slider-model-high\')]')))
+        self.max_price = int(max_price_label.text.strip())
+        
+        self.driver.implicitly_wait(3.0)
+
+    def all_products_within_price_range(self) -> bool:
+        product_prices: list[WebElement] = WebDriverWait(self.driver, 3.0).until(visibility_of_all_elements_located((By.XPATH, '//*[@data-test=\'product-price\']')))
+        return all(float(x.text[1:]) <= self.max_price for x in product_prices)
+    
+    def select_products_category(self) -> None:
+        category_input: WebElement = WebDriverWait(self.driver, 3.0).until(visibility_of_element_located((By.XPATH, '//label[contains(text(), \'Power Tools\')]/input')))
+        category_input.click()
+    
+    def contains_product_with_name(self, name: str) -> bool:
+        WebDriverWait(self.driver, 3.0).until(visibility_of_element_located((By.XPATH, '//div[@data-test=\'filter_completed\']')))
+
+        product_titles: list[WebElement] = WebDriverWait(self.driver, 3.0).until(visibility_of_all_elements_located((By.XPATH, '//*[@data-test=\'product-name\']')))
+        return any(name in x.text.lower() for x in product_titles)
